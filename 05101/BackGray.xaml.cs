ce0818b994069a333f28949d7e88a46ee25264c1
DESKTOP-VKVNN5G\User <A106082014@mail.shu.edu.tw>
2018-06-03 17:23:45 +0800

    背景顏色

---
+++ 05101/BackGray.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _05101
+{
+    /// <summary>
+    /// BackGray.xaml 的互動邏輯
+    /// </summary>
+    public partial class BackGray : UserControl
+    {
+        public BackGray()
+        {
+            InitializeComponent();
+        }
+
+        private void Gray_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+        }
+    }
+}
--- 05101/MainWindow.xaml.cs
+++ 05101/MainWindow.xaml.cs
@@ -27,18 +27,7 @@ namespace _05101
 
         private void SaveBtn_Click(object sender, RoutedEventArgs e)
         {
-            // 產生儲存檔案視窗
-            Microsoft.Win32.SaveFileDialog dig = new Microsoft.Win32.SaveFileDialog();
-
-            // 顯示視窗
-            Nullable<bool> result = dig.ShowDialog();
-
-            // 當按下儲存之後的反應
-            if(result == true)
-            {
-                // 讀檔(路徑)
-               System.IO.File.WriteAllText(dig.FileName, TextArea.Text);
-            }
+          
         }
 
         private void OpenBtn_Click(object sender, RoutedEventArgs e)
@@ -81,17 +70,39 @@ namespace _05101
 
         private void MiddleBtn_Click(object sender, RoutedEventArgs e)
         {
+            // 改變字體大小
             TextArea.FontSize = 25;
         }
 
         private void BigBtn_Click(object sender, RoutedEventArgs e)
         {
+            // 改變字體大小
             TextArea.FontSize = 35;
         }
 
         private void SmallBtn_Click(object sender, RoutedEventArgs e)
         {
+            // 改變字體大小
             TextArea.FontSize = 15;
+        }
+
+        private void Gray_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void BackGray_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // 改變背景與字體顏色
+            TextArea.Background = Brushes.DimGray;
+            TextArea.Foreground = Brushes.White;
+        }
+
+        private void BackWhite_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // 改變背景與字體顏色
+            TextArea.Background = Brushes.White;
+            TextArea.Foreground = Brushes.Black;
         }
     }
 }
